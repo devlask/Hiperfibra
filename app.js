165a3739e2a525a0a89d5b205425c77db10e64b4
devlask <junior159732846@gmail.com>
2023-02-06 10:28:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+// Identificar o clique no menu
+// Verificar o item que foi clicado e fazer referência com o alvo
+// Verificar a distância entre o alvo e o topo
+// Animar o scroll até o alvo
+
+
+// Scroll suave para link interno
+$('.navbar a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('.navbar').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
+
+// Debounce do Lodash
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Animação ao Scroll
+(function(){
+	var $target = $('.anime'),
+			animationClass = 'anime-start',
+			offset = $(window).height() * 3/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
+
+
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 200) {
+		$('.go-top').fadeIn(200);
+		  } else {
+			  $('.go-top').fadeOut(200);
+		 }
+	  });   
+		// Animate the scroll to top
+	  $('.go-top').click(function(event) {
+		event.preventDefault();
+	  $('html, body').animate({scrollTop: 0}, 300);
+  });
